66139b1da01801db36e79b585a1e3e6466409ba3
Oxunjon Ergashev <oxunjonergashev803@gmail.com>
2026-01-27 20:19:55 +0500

    Update project-data.js

--- projects/project-data.js
+++ projects/project-data.js
@@ -8,15 +8,15 @@ window.PROJECTS = [
     live: "https://ergashev-oxunjon.github.io/Ergashev-Oxunjon/",
     repo: "https://github.com/Ergashev-Oxunjon/Ergashev-Oxunjon"
   },
-  {
-    id: "admin",
-    title: "Bootstrap Dashboard",
-    category: ["frontend", "bootstrap"],
-    description: "Sidebar, cards, responsiv dashboard layout. UI strukturasi va component yondashuv.",
-    tech: ["Bootstrap", "JavaScript"],
-    live: "https://ergashev-oxunjon.github.io/DashboardKit/",
-    repo: "https://ergashev-oxunjon.github.io/DashboardKit"
-  },
+ {
+  id: "admin",
+  title: "Bootstrap Dashboard",
+  category: "bootstrap",
+  description: "Sidebar, cards, responsiv dashboard layout. UI strukturasi va component yondashuv.",
+  tech: ["Bootstrap", "JavaScript"],
+  demo: "https://ergashev-oxunjon.github.io/DashboardKit/",
+  github: "https://github.com/ergashev-oxunjon/DashboardKit"
+},
   {
     id: "job-app",
     title: "Job Application UI",
@@ -36,5 +36,6 @@ window.PROJECTS = [
     repo: "#"
   }
 ];
+
 
 
