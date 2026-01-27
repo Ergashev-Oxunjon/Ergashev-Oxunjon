58eda69100bbdaf40468be49352962250a76b23f
Oxunjon Ergashev <oxunjonergashev803@gmail.com>
2026-01-27 20:45:17 +0500

    Update project-data.js

--- projects/project-data.js
+++ projects/project-data.js
@@ -15,7 +15,8 @@ window.PROJECTS = [
   description: "Sidebar, cards, responsiv dashboard layout. UI strukturasi va component yondashuv.",
   tech: ["Bootstrap", "JavaScript"],
   demo: "https://ergashev-oxunjon.github.io/DashboardKit/",
-  github: "https://github.com/ergashev-oxunjon/DashboardKit"
+  github: "https://github.com/ergashev-oxunjon/DashboardKit",
+  image: "assets/img/image_2026-01-27_20-35-43.png"
 },
   {
     id: "job-app",
@@ -24,9 +25,11 @@ window.PROJECTS = [
     description: "Forma, validatsiya, clean UI va foydalanuvchi flow’iga e’tibor qilingan interface.",
     tech: ["HTML", "CSS"],
     demo: "https://ergashev-oxunjon.github.io/Job_application_UI/",
-    github: "https://ergashev-oxunjon.github.io/Job_application_UI"
+    github: "https://ergashev-oxunjon.github.io/Job_application_UI",
+    image: "assets/img/image_2026-01-27_20-42-11.png"
   }
 ];
+
 
 
 
